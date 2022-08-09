fe4f3c8c60b2bd336e27d10f3b865d6b3c494bf7
jleewebdev <lee45276@gmail.com>
2022-08-09 16:12:02 +0800

    Refactor using async and awit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('form');
 const imgDiv = document.querySelector('img');
-const API_KEY =  'REPLACE ME';
+const API_KEY = 'REPLACE ME';
 const errorMessageContainer = document.querySelector('#error-message-container');
 
 form.addEventListener('submit', (e) => {
@@ -9,20 +9,35 @@ form.addEventListener('submit', (e) => {
   search(searchTerm);
 })
 
-const search = (searchTerm) => {
+// Using async and await
+
+const search = async (searchTerm) => {
   hideErrorMessage();
-  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${searchTerm}`, {mode: 'cors'})
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    imgDiv.src = response.data.images.original.url;
-  })
-  .catch((errorMessage) => {
-    console.log(errorMessage);
-    displayErrorMessage("Error. Please try again");
-  });
+  try  {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${searchTerm}`, {mode: 'cors'});
+    const catData = await response.json();
+    imgDiv.src = catData.data.images.original.url;
+  } catch (error) {
+      console.log(error);
+      displayErrorMessage("Error. Please try again");
+  }
 }
+
+// Using .then()
+// const search = (searchTerm) => {
+//   hideErrorMessage();
+//   fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${searchTerm}`, {mode: 'cors'})
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(response) {
+//     imgDiv.src = response.data.images.original.url;
+//   })
+//   .catch((errorMessage) => {
+//     console.log(errorMessage);
+//     displayErrorMessage("Error. Please try again");
+//   });
+// }
 
 const displayErrorMessage = (errorMessage) => {
   errorMessageContainer.classList.remove('hidden');
